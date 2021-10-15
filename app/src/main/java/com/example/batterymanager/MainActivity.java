c32b716aa1abce0d89a02ff7c6ddfd8d8faafb3d
Bharath Jawahar <bharathjawahar49@gmail.com>
2021-10-15 09:15:50 +0530

    Alligned Battery %

--- app/src/main/java/com/example/batterymanager/MainActivity.java
+++ app/src/main/java/com/example/batterymanager/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.batterymanager;
 
+import static java.lang.Thread.sleep;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.BatteryManager;
@@ -7,22 +9,23 @@ import android.os.Bundle;
 import android.service.autofill.TextValueSanitizer;
 import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
     BatteryManager batteryManager;
-    TextView batterPercentageTextView = findViewById(R.id.batteryPercentage);
-
+    TextView batterPercentageTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        batterPercentageTextView = findViewById(R.id.batteryPercentage);
         batteryManager = (BatteryManager)getSystemService(BATTERY_SERVICE);
         int batteryPercentage = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY);
 
-        batterPercentageTextView.setText(batteryPercentage);
+        batterPercentageTextView.setText(batteryPercentage + "%");
     }
 }
