53a4613f5088cbd54940e3d821bd6ab9a4b724ca
Bharath Jawahar <bharathjawahar49@gmail.com>
2021-11-15 08:03:15 +0530

    Daily Sync

--- app/src/main/java/com/example/batterymanager/MainActivity.java
+++ app/src/main/java/com/example/batterymanager/MainActivity.java
@@ -19,7 +19,8 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView batterPercentageTextView, batteryVoltageTextView, batteryStatus;
+    TextView batterPercentageTextView, batteryStatus;
+
     BroadcastReceiver batteryBroadcastReceiver;
     IntentFilter intentFilter;
     @Override
@@ -39,56 +40,21 @@ public class MainActivity extends AppCompatActivity {
                 if(Intent.ACTION_BATTERY_CHANGED.equals(intent.getAction())) {
                     batterPercentageTextView = findViewById(R.id.batteryPercentage);
                     batteryStatus = findViewById(R.id.batteryStatus);
-                    batteryVoltageTextView = findViewById(R.id.batteryVoltage);
 
                     int batteryPercentage = intent.getIntExtra("level", 0);
-                    double voltage = intent.getIntExtra("voltage", 0) * 0.001;
-                    int batteryHealthValue = intent.getIntExtra("health", 0);
-                    String batteryType = intent.getStringExtra("technology");
-                    String batteryHealth = "Good";
-                    switch (batteryHealthValue) {
-                        case BatteryManager.BATTERY_HEALTH_GOOD:
-                            batteryHealth = "Good";
-                            batteryStatus.setBackgroundResource(R.drawable.battery_percentage_background_green);
-                            break;
-                        case BatteryManager.BATTERY_HEALTH_UNSPECIFIED_FAILURE:
-                            batteryHealth = "Failure";
-                            batteryStatus.setBackgroundResource(R.drawable.battery_percentage_background_red);
-                            break;
-                        case BatteryManager.BATTERY_HEALTH_DEAD:
-                            batteryHealth = "Dead";
-                            batteryStatus.setBackgroundResource(R.drawable.battery_percentage_background_red);
-                            break;
-                        case BatteryManager.BATTERY_HEALTH_OVER_VOLTAGE:
-                            batteryHealth = "Over Voltage";
-                            batteryStatus.setBackgroundResource(R.drawable.battery_percentage_background_orange);
-                            break;
-                        case BatteryManager.BATTERY_HEALTH_OVERHEAT:
-                            batteryHealth = "Over Heated";
-                            batteryStatus.setBackgroundResource(R.drawable.battery_percentage_background_orange);
-                            break;
-                        case BatteryManager.BATTERY_HEALTH_UNKNOWN:
-                            batteryHealth = "Unknown";
-                            batteryStatus.setBackgroundResource(R.drawable.battery_percentage_background_orange);
-                            break;
-                    }
                     String batteryPercentageText = batteryPercentage + "%";
-
                     batterPercentageTextView.setText(batteryPercentageText);
-                        if(batteryPercentage>80)             batterPercentageTextView.setBackgroundResource(R.drawable.battery_percentage_background_green);
-                        else if(batteryPercentage>50)        batterPercentageTextView.setBackgroundResource(R.drawable.battery_percentage_background_blue);
-                        else if(batteryPercentage>25)        batterPercentageTextView.setBackgroundResource(R.drawable.battery_percentage_background_orange);
-                        else                                 batterPercentageTextView.setBackgroundResource(R.drawable.battery_percentage_background_red);
-                    batteryStatus.setText(batteryHealth);
-                    batteryVoltageTextView.setText(String.valueOf(voltage) + " v");
 
-                    System.out.println(BatteryManager.BATTERY_STATUS_CHARGING);
-
-                    if(BatteryManager.BATTERY_STATUS_CHARGING == 2 && batteryPercentage == 100) {
-                        Uri alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-                        MediaPlayer mp = MediaPlayer.create(getApplicationContext(), alarmSound);
-                        mp.start();
+                    if(BatteryManager.BATTERY_STATUS_CHARGING == 2) {
+                        if(intent.getIntExtra("level", 0) == 100) {
+                            batteryStatus.setText("Charged");
+                        }
+                        else {
+                            batteryStatus.setText("Charging");
+                        }
                     }
+                    else    batteryStatus.setText("Discharging");
+
                 }
             }
         };
